cb677ed1c425d220c29e5364801ebd2c51a90855
Anmol Singh <anmolpratapsingh1997@gmail.com>
2025-08-15 01:29:08 +0530

    Update randomId format

--- src/shared.ts
+++ src/shared.ts
@@ -5,7 +5,7 @@ export const isNull = (v: unknown): v is undefined | null => {
 };
 
 export const randomId = () => {
-  return 'ts-edge-xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return 'dume_xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0;
     const v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
